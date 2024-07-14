7bbce0e2ab1ad613227635965fc067a25583e24c
Igor Stroganov <igor.stroganov@pandadoc.com>
2024-07-15 01:06:10 +0200

    Migrate to gulp 5

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,54 @@
-'use strict';
+import gulp from "gulp";
+import gulpSass from "gulp-sass";
+import * as dartSass from "sass";
+import { deleteAsync } from "del";
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const del = require('del');
+const sass = gulpSass(dartSass);
 
 const paths = {
-    html: './src/index.html',
-    sass: './src/sass/**/*.scss',
-    js: './src/js/**/*.js',
-    audio: './src/audio/**/*'
+  html: "./src/index.html",
+  sass: "./src/sass/**/*.scss",
+  js: "./src/js/**/*.js",
+  audio: "./src/audio/**/*",
 };
-const outputDir = './dist';
 
-gulp.task('default', ['build']);
+const outputDir = "./dist";
 
-gulp.task('build', ['sass', 'static']);
+function buildSass() {
+  const sassOptions = { outputStyle: "compressed" };
 
-gulp.task('watch', ['build'], () => {
-    gulp.watch(paths.sass, ['sass']);
-    gulp.watch([paths.html, paths.js], ['static']);
-});
+  return gulp
+    .src(paths.sass)
+    .pipe(sass(sassOptions).on("error", sass.logError))
+    .pipe(gulp.dest(outputDir + "/css"));
+}
 
-gulp.task('sass', () => {
-    const sassOptions = { outputStyle: 'compressed' };
-    return gulp.src(paths.sass)
-        .pipe(sass(sassOptions).on('error', sass.logError))
-        .pipe(gulp.dest(outputDir + '/css'));
-});
+function html() {
+  return gulp.src(paths.html).pipe(gulp.dest(outputDir));
+}
 
-gulp.task('static', () => {
-    gulp.src(paths.html).pipe(gulp.dest(outputDir));
-    gulp.src(paths.js).pipe(gulp.dest(outputDir + '/js'));
-    gulp.src(paths.audio).pipe(gulp.dest(outputDir + '/audio'));
-});
+function js() {
+  return gulp.src(paths.js).pipe(gulp.dest(outputDir + "/js"));
+}
 
-gulp.task('clean', () => {
-    return del('dist/**/*');
-});
+function audio() {
+  return gulp.src(paths.audio).pipe(gulp.dest(outputDir + "/audio"));
+}
+
+export const buildStatic = gulp.parallel(html, js, audio);
+
+function watchSass() {
+  return gulp.watch(paths.sass, buildSass);
+}
+
+function watchStatic() {
+  return gulp.watch([paths.html, paths.js], buildStatic);
+}
+
+export const watch = gulp.parallel(watchSass, watchStatic);
+
+export function clean() {
+  return deleteAsync("dist/");
+}
+
+export default gulp.parallel(buildSass, buildStatic);
